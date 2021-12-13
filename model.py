46033969fa8dc72483bbd68f2a9e6945bbc9315e
guanghuhappysf128 <ghu1@stuednt.unimelb.edu.au>
2021-12-14 04:50:45 +1100

    init

---
+++ model.py
@@ -0,0 +1,201 @@
+import os
+import copy
+
+
+# Class of the problem
+
+class Problem():
+    initial_state = {}
+    actions = {} 
+    entities = {} # agent indicators, should be unique
+    variables = {} #variable
+    domains = {}
+    initial_state = {}
+    goal_states = {}
+
+    def __init__(self, entities, variables, actions,domains,initial_state, goal_states):
+        self.entities = entities
+        self.actions = actions
+        self.variables = variables
+        self.domains = domains
+        self.initial_state = initial_state
+        self.goal_states = goal_states
+    
+    def isGoal(self,state):
+        for k,i in self.goal_states.items():
+            if not state[k] == i:
+                return False
+        return True
+    
+    def getLegalActions(self,state):
+        legal_actions = {}
+        for a_name, a in self.actions.items():
+            print(f"param: {a.a_parameters}; all params: {self._generateParams(a.a_parameters)}")
+            for params in self._generateParams(a.a_parameters):
+                for i,v in params:
+                    a_temp_name = a_name
+                    a_temp_parameters = copy.deepcopy(a.a_parameters)
+                    print(f"a's PPPPP {a.a_parameters}")
+                    print(a)
+                    print((i,v))
+                    a_temp_precondition = copy.deepcopy(a.a_precondition)
+                    a_temp_effects = copy.deepcopy(a.a_effects)
+                    a_temp_name = a_temp_name + "-" + v
+                    for j in range(len(a_temp_parameters)):
+                        v_name, v_effects = a_temp_parameters[j]
+                        v_name = v_name.replace(f'{i}',f'?{v}')
+                        a_temp_parameters[j] = (v_name,v_effects)
+                    for j in range(len(a_temp_precondition)):
+                        v_name, v_effects = a_temp_precondition[j]
+                        v_name = v_name.replace(f'{i}',f'?{v}')
+                        v_effects = v_effects.replace(f'{i}',f'?{v}')
+                        a_temp_precondition[j] = (v_name,v_effects)
+                    for j in range(len(a_temp_effects)):
+                        v_name, v_effects = a_temp_effects[j]
+                        v_name = v_name.replace(f'{i}',f'?{v}')
+                        v_effects = v_effects.replace(f'{i}',f'?{v}')
+                        a_temp_effects[j] = (v_name,v_effects)
+                    # TODO: adding precondition check
+                    legal_actions.update({a_temp_name:Action(a_temp_name,a_temp_parameters,a_temp_precondition,a_temp_effects)})
+                    print(legal_actions)
+        return legal_actions
+    
+    def _generateParams(self,params):
+        param_list = []
+        print(params)
+        if params == []:
+            return []
+        else:
+            i,v = params[0]
+            print((i,v))
+            for k,l in self.entities.items():
+                print(l)
+                if l.e_type == v:
+                    next_param = copy.deepcopy(params[1:])
+                    rest = self._generateParams(next_param)
+                    if len(rest) == 0:
+                        param_list = param_list + [[(i,k)]]
+                    else:
+                        param_list = param_list + [ [(i,k)]+ t for t in self._generateParams(next_param) ]
+        return param_list
+                    
+    # TODO adding action cost
+    def generatorSuccessor(self,state,action):
+        
+        # TODO valid action
+        # need to go nested on the brackets
+        
+        new_state = copy.deepcopy(state)
+        print(action)
+        for v_name,update in action.a_effects:
+            v_name = v_name.replace('?','-')
+            if '-' in update:
+                v2_name,value = update.split('-')
+                v2_name = v2_name.replace('?','-')
+                v2_value = state[v2_name]
+                domain_name = self.variables[v_name].v_domain_name
+                if self.domains[domain_name].d_type == D_TYPE.ENUMERATE:
+                    for index, item in enumerate(self.domains[domain_name].d_values):
+                        if item == v2_value:
+                            break
+                    new_state[v_name] = self.domains[domain_name].d_values[(index-int(value))%len(self.domains[domain_name].d_values)]
+            elif '+' in update:
+                v2_name,value = update.split('+')
+                v2_name = v2_name.replace('?','-')
+                v2_value = state[v2_name]
+                domain_name = self.variables[v_name].v_domain_name
+                if self.domains[domain_name].d_type == D_TYPE.ENUMERATE:
+                    for index, item in enumerate(self.domains[domain_name].d_values):
+                        if item == v2_value:
+                            break
+                    new_state[v_name] = self.domains[domain_name].d_values[(index+int(value))%len(self.domains[domain_name].d_values)]
+            elif '=' in update:
+                pass
+        
+        return new_state
+        
+        
+    
+    def __str__(self):
+        return f"Problem: \n\t entities: {self.entities}\n\t variables: {self.variables}\n\t actions: {self.actions}\n\t types: {self.types}\n\t initial_state: {self.initial_state}\n\t goal_states: {self.goal_states}\n"
+
+from enum import Enum
+class E_TYPE(Enum):
+    AGENT = 1
+    OBJECT = 2
+   
+class Entity():
+    e_name = None
+    e_type = None
+   
+    def __init__(self,e_name, e_type):
+        self.e_name = e_name
+        self.e_type = e_type
+
+    def __str__(self): # show only in the print(object)
+        return f"<Entity: name: {self.e_name}; type: {self.e_type}>\n"
+
+    def __repr__(self): # show when in a dictionary
+        return f"<Entity: name: {self.e_name}; type: {self.e_type}>\n"
+
+class Action():
+    a_name = None
+    a_parameters = []
+    a_precondition = None
+    a_effects = None
+    
+    def __init__(self,a_name, a_parameters, a_precondition, a_effects):
+        self.a_name = a_name
+        self.a_parameters = a_parameters
+        self.a_precondition = a_precondition
+        self.a_effects = a_effects
+
+    def __str__(self): # show only in the print(object)
+        return f"<Action: {self.a_name}; parameters: {self.a_parameters}; precondition: {self.a_precondition}; effects: {self.a_effects}>\n"
+
+    def __repr__(self): # show when in a dictionary
+        return f"<Action: {self.a_name}; parameters: {self.a_parameters}; precondition: {self.a_precondition}; effects: {self.a_effects}>\n"
+    
+class Variable():
+    v_name = None
+    v_domain_name = None
+    v_parent = None
+    
+    def __init__(self,name,domain_name,v_parent):
+        self.v_name = name
+        self.v_domain_name = domain_name
+        self.v_parent = v_parent
+        
+    def __str__(self): # show only in the print(object)
+        return f"<Variable: name: {self.v_name}; domain: {self.v_domain_name}; parent: {self.v_parent}>\n"
+
+    def __repr__(self): # show when in a dictionary
+        return f"<Variable: name: {self.v_name}; domain: {self.v_domain_name}; parent: {self.v_parent}>\n"
+        
+        
+        
+class D_TYPE(Enum):
+    ENUMERATE = 1
+    INTEGER = 2 
+           
+class Domain():
+    d_name = None
+    d_values = None
+    d_type = None
+    agency = False
+    
+    def __init__(self,d_name,d_values,agency,d_type):
+        self.d_name = d_name
+        self.d_values = d_values
+        self.agency = agency
+        self.d_type = d_type
+    
+    def __str__(self): # show only in the print(object)
+        return f"<domain_name: {self.d_name}; Basic type: {self.d_type}; values: {self.d_values}; isAgent?: {self.agency}>\n"
+
+    def __repr__(self): # show when in a dictionary
+        return f"<domain_name: {self.d_name}; Basic type: {self.d_type}; values: {self.d_values}; isAgent?: {self.agency}>\n"
+    
+    def isAgent(self):
+        return self.agency
+    
